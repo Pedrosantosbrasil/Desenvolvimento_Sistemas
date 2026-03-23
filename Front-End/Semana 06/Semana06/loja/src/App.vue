41eb5be947d91fa92aad172e35dc6d19bad1b732
Pedro <pedrosantos0514@gmail.com>
2026-03-23 08:12:12 -0300

    Projeto VUEX

---
+++ Front-End/Semana 06/Semana06/loja/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ShoppingCart />
+</template>
+
+<script>
+import ShoppingCart from "./components/ShoppingCart.vue";
+
+export default {
+  components: {
+    ShoppingCart,
+  },
+};
+</script>
